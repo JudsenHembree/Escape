da7115a5c121f9ac82389a697a8ec4c71b325d18
Judsen S Hembree <jhembre@g.clemson.edu>
2022-03-14 23:18:27 -0400

    Clean ouija board lerps, teleporting, and keeping track of ouija sequence
    TODO1: Now that we have the word we can pass that into an array of words and do different things based off different words. TODO2: Add some non eye based vr components to the escape room. TODO3: The angle of ray meme. TODO4: The actual escape!

--- Assets/eyeTracking.cs
+++ Assets/eyeTracking.cs
@@ -5,26 +5,40 @@ using ViveSR.anipal.Eye;
 public class eyeTracking: MonoBehaviour
 {
     private static Ray testRay;
+    //eye memes
     private static FocusInfo focusInfo;
+    //eye memes
 
-    //the ouija board object
     public GameObject board;
+    //the ouija board object
+
     public float lerpDuration;
+    //how long lerp last
     private float timeElapsed;
+    //used in lerp
+    public GameObject[] objectsOfInterest;
     //objects of whose interest we track
     //namely the letters on board
-    public GameObject[] objectsOfInterest;
-    //current location of ouija key. updated throughout execution
     private Transform keyLocation;
+    //current location of ouija key. updated throughout execution
     private Transform keyLocationResting;
+    //used as varable startlocation for lerps
 
-    //where key is headed
-    //will be home unless player is looking at letter
     private Transform lerpDest;
-    //original location of key. It's like the center of the board
+    //where key is headed
+    //will be current keyLocation unless player is looking at letter
     private Transform home;
+    //original location of key. It's like the center of the board
+
 
     private bool lerpin;
+    //used to prevent overlapping lerps. makes shit jump around no good. 
+
+    private string ouijaWord; 
+    //collect sequence entered on ouija board to pass to an array of words, 
+    //call a func based of sequence
+    private string[] commands;
+    //sequence of commands
 
 
 
@@ -42,7 +56,7 @@ public class eyeTracking: MonoBehaviour
     void Update()
     {
         interest(objectsOfInterest);
-        lerpDest = determineLerpDest(objectsOfInterest);
+        determineLerpDest(objectsOfInterest);
         lerpandslerp();
         sendKeyHome();
     }
@@ -72,6 +86,7 @@ public class eyeTracking: MonoBehaviour
         }  
     }
 
+    //func that determines if player has stopped looking at board
     private bool noInterest(GameObject[] objects){
         foreach(GameObject meme in objects){
             if (meme.GetComponent<interestLvl>().interest != 1){
@@ -81,17 +96,34 @@ public class eyeTracking: MonoBehaviour
         return true;
     }
 
+    //if we ain't lerpin or looking send key to home
     private void sendKeyHome(){
-        if(noInterest(objectsOfInterest)){
-            if(keyLocation != home){
-                lerpDest = home;
-                lerpin = true;
-                StartCoroutine(lerp());
+        if(!lerpin){
+            if(noInterest(objectsOfInterest)){
+                if(keyLocation.position != home.position){
+                    checkWordForCommand(commands);
+                    lerpDest = home;
+                    ouijaWord="";
+                    lerpin = true;
+                    StartCoroutine(lerp());
+                }
             }
         }
     }
 
+    //check ouija word against list of commands then do stuff
+    private void checkWordForCommand(string[] x){
+        if(!string.IsNullOrEmpty(ouijaWord)){
+            Debug.Log("word is currently, "+ouijaWord);
+        }
+        if(ouijaWord=="test"){
+            Debug.Log("word checks out");
+        }
+        //do stuff
+    }
 
+
+    //calulates interest metric for each object we care about
     public void interest(GameObject[] objects){
         GameObject temp = Focus();
         if (temp != null){
@@ -114,26 +146,31 @@ public class eyeTracking: MonoBehaviour
             }
         }
     }
-    private Transform determineLerpDest(GameObject[] objects){
-        Transform lerpDest = keyLocation;
-        foreach(GameObject meme in objects){
-            if(meme.GetComponent<interestLvl>() == null){
-                Debug.Log("There should be an interestLvl script attached to object " + meme.name);
-            }else{
-                //probably need to adjust for multiple letters over the twosecondmark
-                if(meme.GetComponent<interestLvl>().interest>=meme.GetComponent<interestLvl>().twoSecondMark){
-                    //Debug.Log("setting new lerp dest " + meme);
-                    string name = meme.name;
-                    lerpDest = meme.transform.parent.parent.Find("key destinations").Find(name);
+
+    //determines ouija key destination based of interest metric
+    private void determineLerpDest(GameObject[] objects){
+        if(!lerpin){
+            lerpDest = keyLocation;
+            foreach(GameObject meme in objects){
+                if(meme.GetComponent<interestLvl>() == null){
+                    Debug.Log("There should be an interestLvl script attached to object " + meme.name);
+                }else{
+                    //probably need to adjust for multiple letters over the twosecondmark
+                    if(meme.GetComponent<interestLvl>().interest>=meme.GetComponent<interestLvl>().twoSecondMark){
+                        //Debug.Log("setting new lerp dest " + meme);
+                        string name = meme.name;
+                        lerpDest = meme.transform.parent.parent.Find("key destinations").Find(name);
+                    }
                 }
             }
+            //Debug.Log("Final lerp dest " + lerpDest.gameObject);
         }
-        Debug.Log("Final lerp dest " + lerpDest.gameObject);
-        return lerpDest;
     }
-    //TODO: set up the actual lerp
+
+    //moves the key also manages the ouija word sequence
     private IEnumerator lerp(){
-        lerpin = true;
+        Debug.Log("lerpin");
+        addLetter();
         timeElapsed=0;
         while(timeElapsed<lerpDuration){
             keyLocation.position=Vector3.Lerp(keyLocationResting.position,lerpDest.position,timeElapsed/lerpDuration);
@@ -143,16 +180,25 @@ public class eyeTracking: MonoBehaviour
         keyLocation.position = lerpDest.position;
         setResting();
         lerpin = false;
+        
     }
 
+    //lerpandslerpbby... call this in update to start lerps
     private void lerpandslerp(){
         if(keyLocation.position != lerpDest.position && !lerpin){
-            Debug.Log("lerpin");
+            lerpin = true;
             StartCoroutine(lerp());
         }
     }
 
+    //append ouija word
+    private void addLetter(){
+        if(lerpDest.name!="ouija home" && lerpDest.name != "ouija key"){
+            ouijaWord+=lerpDest.name;
+        }
+    }
 
+    //set resting ouija key position
     private void setResting(){
         keyLocationResting=keyLocation;
     }
