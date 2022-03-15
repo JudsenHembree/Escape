edcda964a444cd0e9568fd898014016bb7616193
Judsen S Hembree <jhembre@g.clemson.edu>
2022-03-15 14:42:30 -0400

    ui for ouija
    sprite is updated with an exponential function so the ui is kinda gross atm

--- Assets/interestLvl.cs
+++ Assets/interestLvl.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class interestLvl : MonoBehaviour
 {
     // Start is called before the first frame update
     public int interestX;
     public double interest;
+    public Image progress;
 
     public double twoSecondMark;
     void Start()
@@ -23,12 +25,20 @@ public class interestLvl : MonoBehaviour
     }
 
     private void interestFunc(){
-        interest = 1*(Math.Pow(Math.E,(0.01*interestX)));  
+        interest = 1*(Math.Pow(Math.E,(0.1*interestX)));  
+        updateSprite();
+    }
+
+    //janky ui for showing a person they are selecting a letter
+    private void updateSprite(){
+        if(progress != null){
+            progress.fillAmount = (float)interest/(float)twoSecondMark;
+        }
     }
 
     //1 second continuous gaze @ 90 frames per second
     private double checkValue(){
-        return (1*(Math.Pow(Math.E,(0.01*90))));
+        return (1*(Math.Pow(Math.E,(0.1*90))));
     }
 
     public void grow(){
